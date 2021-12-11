604eb44a1db6dca161f89cd0b4990f9fcf199f36
stiamat <38559112+stiamat@users.noreply.github.com>
2021-12-11 14:41:49 -0300

    modal funcionando

--- src/main/webapp/app/layouts/search/search.component.ts
+++ src/main/webapp/app/layouts/search/search.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'jhi-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.scss'],
 })
-export class SearchComponent implements OnInit {
-  constructor() {}
+export class SearchComponent implements OnInit, AfterViewInit {
+  tabSelectedSearch: number;
+  totalTabSearch: number;
+  resultSearch: any;
+  @ViewChild('modal') modal: TemplateRef<any>;
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    console.warn('teste');
+    this.totalTabSearch = 3;
+    this.tabSelectedSearch = 0;
+  }
+
+  ngAfterViewInit(): void {
+    console.warn(this.modal);
+    this.openModal();
+  }
+
+  openModal(): void {
+    const modal = this.dialog.open(this.modal, {
+      width: '80%',
+      disableClose: true,
+      closeOnNavigation: true,
+    });
+
+    modal.afterClosed().subscribe(result => {
+      console.warn(result);
+    });
+  }
+
+  selectedNextTabSearch(): void {
+    this.tabSelectedSearch = (this.tabSelectedSearch + 1) % this.totalTabSearch;
+  }
+  selectedPreviesTabSearch(): void {
+    this.tabSelectedSearch = (this.tabSelectedSearch - 1) % this.totalTabSearch;
   }
 }
